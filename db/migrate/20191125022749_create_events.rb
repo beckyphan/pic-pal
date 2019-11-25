8e4c84ac14061a4feab5a248bd5d18fdf3b7d223
Becky Phan <beckyphan@live.com>
2019-11-24 22:05:56 -0500

    corrected event migration/schema

--- db/migrate/20191125022749_create_events.rb
+++ db/migrate/20191125022749_create_events.rb
@@ -3,7 +3,7 @@ class CreateEvents < ActiveRecord::Migration[6.0]
     create_table :events do |t|
       t.string :title
       t.datetime :datetime
-      t.belongs_to :host, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :movie, null: false, foreign_key: true
 
       t.timestamps
