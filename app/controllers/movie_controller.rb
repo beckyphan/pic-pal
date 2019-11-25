08c0e3a46ac80ce7d3e582ea2ad88244bb62011a
Becky Phan <beckyphan@live.com>
2019-11-24 21:25:41 -0500

    added Movie model and controller

---
+++ app/controllers/movie_controller.rb
@@ -0,0 +1,22 @@
+class MovieController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'movie/index'
+  get 'movie/home'
+  get 'movie/edit'
+  get 'movie/update'
+  get 'movie/create'
+  get 'movie/show'
+  get 'movie/delete'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  # root to: "home#index"
+
+  root to: "movies#home"
 end
