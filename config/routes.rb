8f6fe6f44cf956796406a8dd2ee39dee7a4dcc7c
Becky Phan <beckyphan@live.com>
2019-11-24 22:14:47 -0500

    generated devise:views users

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
+  root to: "movies#home"
   resources :comments
   resources :events
-  get 'movie/index'
-  get 'movie/home'
-  get 'movie/edit'
-  get 'movie/update'
-  get 'movie/create'
-  get 'movie/show'
-  get 'movie/delete'
+  resources :movies
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root to: "movies#home"
 end
