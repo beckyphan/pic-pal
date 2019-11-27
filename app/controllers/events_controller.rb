6f29803cdce2c726625f8252ea979f13081a2438
Becky Phan <beckyphan@live.com>
2019-11-27 09:40:51 -0500

    unfinished - events#new form and event#create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,9 +5,23 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
+    @events = Event.all
   end
 
   def create
+    datetime_formatted = DateTime.new(datetime_params.values[0].to_i,
+      datetime_params.values[1].to_i,
+      datetime_params.values[2].to_i,
+      datetime_params.values[3].to_i,
+      datetime_params.values[4].to_i)
+    @event = Event.new(event_params, datetime: datetime_formatted)
+
+    if @event.save!
+      redirect_to movie_path(@event.movie_id)
+    else
+      render "new"
+    end
   end
 
   def edit
@@ -17,5 +31,15 @@ class EventsController < ApplicationController
   end
 
   def destroy
-  end 
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :location, :host_id, :movie_id)
+  end
+
+  def datetime_params
+    params.require(:event).permit("datetime(1i)", "datetime(2i)", "datetime(3i)", "datetime(4i)", "datetime(5i)")
+  end
 end
