9b6bdf0ce3e4c16851adc63d38e73422e0dd8f4f
Becky Phan <beckyphan@live.com>
2019-11-27 08:47:08 -0500

    updated events#index view and controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,21 @@
 class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end 
 end
