625262eec7bef62c1fe24fc81f1b61dfdda09936
Becky Phan <beckyphan@live.com>
2019-11-27 01:31:30 -0500

    added comments for relationships and validations for review and comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,3 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :event
 end
+
+# need to build out comments
+# event has_many comments
+# user has_many comments
+# comment belongs_to user
+# comment belongs_to event
+# validations: user can only comment on events if they are attending event 
--- app/models/review.rb
+++ app/models/review.rb
@@ -4,3 +4,6 @@ class Review < ApplicationRecord
 
   validates :rating, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
 end
+
+# potential feature: 'watched_list' scope being list of movies user has watched
+# user may not post a review unless they have watched movies
