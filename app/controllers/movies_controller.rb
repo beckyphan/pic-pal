5ea5fea1ac00d58f93c0fd4bdb1abd55ac19f995
Becky Phan <beckyphan@live.com>
2019-11-26 21:02:38 -0500

    updated movies#create and corrected movie#index interation fields

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,18 +10,31 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_path
+    else
+      render "create"
+    end
+  end
+
   def edit
   end
 
   def update
   end
 
-  def create
-  end
-
   def show
   end
 
   def delete
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :genre, :image_url)
   end
 end
