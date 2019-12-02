19b2f431d55735165572fa0040a636e8b4e4be06
Becky Phan <beckyphan@live.com>
2019-12-02 12:46:32 -0800

    added route to add new event from movie#show page

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,10 @@ Rails.application.routes.draw do
   resources :movies, only: [:show] do
     resources :reviews, only: [:new]
   end
+
+  resources :movies, only: [:show] do
+    resources :events, only: [:new]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
