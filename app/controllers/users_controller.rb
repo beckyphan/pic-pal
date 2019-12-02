273c62a580bef094c0b45a301136631ad8d3e12e
Becky Phan <beckyphan@live.com>
2019-12-02 08:50:42 -0800

    updated expected local variables that will be used for user#show in user#controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,12 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
-    binding.pry
+
     @hosted_events = @user.hosted_events
     @attended_events = @user.attended_events
+    
     @reviews = @user.movie_reviews
+    @movies_reviewed = @user.reviewed_movies
   end
 
 end
