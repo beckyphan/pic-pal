5c9291cad5bdf696444776fa01c32771731b9fec
Becky Phan <beckyphan@live.com>
2019-12-02 13:30:23 -0800

    finished guestlist#create method for attending guests

--- app/controllers/guestlists_controller.rb
+++ app/controllers/guestlists_controller.rb
@@ -1,10 +1,18 @@
 class GuestlistsController < ApplicationController
 
   def create
-    binding.pry
+    @attendee = Guestlist.create(attendee_params)
+
+    redirect_to event_path(@attendee.event_id)
   end
 
   def destroy
+  end
+
+  private
+
+  def attendee_params
+    params.require(:guestlist).permit(:event_id, :attendee_id)
   end
 
 end
