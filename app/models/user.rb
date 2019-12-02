a2e9d583e4dea3f89957359cc72ed20a0c962a69
Becky Phan <beckyphan@live.com>
2019-12-02 08:35:52 -0800

    updated user model associations to appropriately pull associated objects for user#show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   has_many :hosted_events, foreign_key: :host_id, class_name: "Event"
   has_many :guestlists, foreign_key: :user_id
-  has_many :attended_events, through: :guestlist, source: :event
+  has_many :attended_events, through: :guestlists, source: :event
 
   has_many :movie_reviews, foreign_key: :review_id, class_name: "Review"
-  has_many :reviewed_movies, through: :reviews, source: :movies
+  has_many :reviewed_movies, through: :movie_reviews, source: :movie
 
   def self.from_omniauth(access_token)
     data = access_token.info
