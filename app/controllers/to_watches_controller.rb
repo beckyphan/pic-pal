53e45529f71a269c437339c907bf4f483c922645
Becky Phan <beckyphan@live.com>
2019-12-02 11:13:48 -0800

    created fix for update to_watch, unclear why unable to use .update in to_watches controller

--- app/controllers/to_watches_controller.rb
+++ app/controllers/to_watches_controller.rb
@@ -1,14 +1,15 @@
 class ToWatchesController < ApplicationController
 
   def create
-    binding.pry
     @watch = ToWatch.create(to_watch_params)
     redirect_to movie_path(@watch.movie_id)
   end
 
   def update
     @towatch = ToWatch.where(movie_id: params[:to_watch][:movie_id], user_id: current_user.id)
-    @towatch.first.update(watched: watched_param)
+    @updated = @towatch.first
+    @updated.watched = params[:to_watch][:watched]
+    @updated.save!
 
     redirect_to movie_path(@towatch.first.movie_id)
   end
@@ -20,6 +21,6 @@ class ToWatchesController < ApplicationController
   end
 
   def watched_param
-    params.require(:to_watch).permit(:watched)
+    params.require(:to_watch).permit(:watched, :movie_id)
   end
 end
